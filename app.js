bdaf702e245a2491bf566d325a14806d6dfaeee6
Smelt <shivamsatyarthi@gmail.com>
2018-04-21 00:19:02 -0400

    comments

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const yahoo = require('yahoo-stocks');
 const finance = require('finance');
 
 
-
+//where we specify the ticker/option date we want to examine
 const opts = {
   symbol: 'AAPL',
   expiration: '2018-05-11'
@@ -21,9 +21,10 @@ function getOptionData(err, keyStats) {
   });
 }
 
-
 function findClosestMoney(quotes) {
+  //this data will ideally be found from API
   const currPrice = 165;
+  //prints the closests in the money call
   quotes.calls.forEach((call) => {
     if (call.strike == currPrice) {
       console.log(`Call  : ${call.strike}`);
@@ -31,20 +32,18 @@ function findClosestMoney(quotes) {
       console.log(`Implied Volatility: ${call.impliedVolatility}`);
     }
   });
-
   console.log();
-
+  //prints the closest in the money put
   quotes.puts.forEach((put) => {
     if (put.strike == currPrice) {
       console.log(`Put  : ${put.strike}`);
       console.log(`Last Price ${put.lastPrice}`);
       console.log(`Implied Volatility: ${put.impliedVolatility}`);
     }
-
   });
-
 }
 
+//formats and prints key primary data this will later be used to calculate Vega
 function printKeyData(stock) {
   console.log(`Institutional investors:  ${stock.heldPercentInstitutions.fmt}`);
   console.log(`Shares short percent of float:  ${stock.shortRatio.fmt}`);
