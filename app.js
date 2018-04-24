d9643bf4f42ba786bc4f293056e7e18737c35ee1
Smelt <shivamsatyarthi@gmail.com>
2018-04-23 23:13:09 -0400

    resolved stocks api

--- app.js
+++ app.js
@@ -6,13 +6,27 @@ const stockService = require('./services/stocks.service');
 const Stock = require('./models/stock');
 const Option = require('./models/option');
 
+const finance = require('finance');
 
+var parts ='2018-04-03'.split('-');
+// Please pay attention to the month (parts[1]); JavaScript counts months from 0:
+// January - 0, February - 1, etc.
+var mydate = new Date(parts[0], parts[1] - 1, parts[2]); 
+
+finance.quotes.getQuotes(['FB'], new Date('2018-04-23'), function(err,symbol,price){
+  console.log(symbol);
+  console.log(price);
+});
+
+/*
 run('aapl', 165, '2018-05-11');
 
 setTimeout(function(){
   console.log();
   run('fb', 165, '2018-05-11');
 }, 1200 );
+
+
 
 
 function run(ticker, price, date){
@@ -22,7 +36,7 @@ function run(ticker, price, date){
   let option = new Option(stock.getSymbol(), stock.getPrice(), date);
   option = optionService.getOptionData(option);
 }
-
+*/
 
 
 
--- services/stocks.service.js
+++ services/stocks.service.js
@@ -1,15 +1,19 @@
 
 const finance = require('finance');
+const yahoo = require('yahoo-nasdaq');
 
 module.exports.getStockData = function(stock){
    const stockParam = {
        symbol: stock.getSymbol()
    }
+
+   
+  // console.log(info);
     finance.keystatistics.getKeyStatistics(stockParam, (err, data) => {
     stock.setHeldInstitutions(data.heldPercentInstitutions.raw);
     stock.setSharesShort(data.shortRatio.raw);
     stock.setBeta(data.beta.raw);
-    stock.printSummary();
+    //stock.printSummary();
    });
 }
 
