8e218c116e664a6f3d86228ba31946243a988ae1
Smelt <shivamsatyarthi@gmail.com>
2018-04-26 22:47:04 -0400

    model for crypto has ability to get price

---
+++ models/crypto.js
@@ -0,0 +1,31 @@
+const cryptoService = require('../services/crypto.service');
+
+class Crypto {
+    
+    constructor(symbol){
+        this.symbol = symbol;
+        this.data = {};
+    }
+
+    getPrice(){
+        return this.price;
+    }
+
+    getSymbol(){
+        return this.symbol;
+    }
+
+    setPrice(price){
+        this.price = price;
+    }
+
+    printSummary(){
+
+    }
+
+    async refreshData(){
+        this.price = await cryptoService.getCryptoPrice(this.symbol);
+    }
+}
+
+module.exports = Crypto;
