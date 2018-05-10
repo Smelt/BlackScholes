a9cdcae8b2cfc5c6c1416903c47037e43c144657
Smelt <shivamsatyarthi@gmail.com>
2018-05-10 00:00:26 -0400

    removed test features

--- services/options.service.js
+++ services/options.service.js
@@ -9,35 +9,29 @@ module.exports.getOptionData = async function (option) {
         expirationDate: option.getExpiration(),
         symbol: option.getSymbol()
     }
-    
-    return new Promise(function(resolve,reject){        
-    finance.optionchain.getOptionChainFromYahoo(opts, function (err, quotes) {
-       option = findClosestMoney(option, quotes); 
-        resolve(option);
-    });
-})
+
+    return new Promise(function (resolve, reject) {
+        finance.optionchain.getOptionChainFromYahoo(opts, function (err, quotes) {
+            option = findClosestMoney(option, quotes);
+            resolve(option);
+        });
+    })
 }
 
 async function findClosestMoney(option, quotes) {
-    
-   // console.log(quotes);
-   const operatedCalls = quotes.calls.map( price => Math.abs(price.strike - option.getStockPrice() ));
-   const callIndex = operatedCalls.reduce((min, x, i, arr) => x < operatedCalls[min] ? i : min, 0);
 
-   const operatedPuts = quotes.puts.map( price => Math.abs(price.strike - option.getStockPrice() ));
-   const putIndex = operatedPuts.reduce((min, x, i, arr) => x < operatedPuts[min] ? i : min, 0);
-   console.log(quotes.calls[callIndex].strike);
-    console.log(quotes.puts[putIndex].strike);
-    quotes.calls.forEach((call) => {
-        if (call.strike == option.getStockPrice()) {
-            option.setCall(call.strike, call.lastPrice, call.impliedVolatility);
-        }
-    });
-    quotes.puts.forEach((put) => {
-        if (put.strike == option.getStockPrice()) {
-            option.setPut(put.strike, put.lastPrice, put.impliedVolatility);
-        }
-    });
+    // console.log(quotes);
+    const operatedCalls = quotes.calls.map(price => Math.abs(price.strike - option.getStockPrice()));
+    const callIndex = operatedCalls.reduce((min, x, i, arr) => x < operatedCalls[min] ? i : min, 0);
+    const call = quotes.calls[callIndex];
+
+    option.setCall(call.strike, call.lastPrice, call.impliedVolatility);
+
+    const operatedPuts = quotes.puts.map(price => Math.abs(price.strike - option.getStockPrice()));
+    const putIndex = operatedPuts.reduce((min, x, i, arr) => x < operatedPuts[min] ? i : min, 0);
+    const put = quotes.puts[putIndex];
+    option.setPut(put.strike, put.lastPrice, put.impliedVolatility);
+
     return option;
 }
 
