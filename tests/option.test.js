dc582737ef961a80d7c6417f550ef40be4005b5f
Smelt <shivamsatyarthi@gmail.com>
2018-05-09 23:42:37 -0400

    moved options Tests together

--- tests/option.test.js
+++ tests/option.test.js
@@ -1,12 +1,19 @@
-module.exports.stockPrice = async function(ticker, price, date){
-    let stock = new Stock(ticker);
-    //await stockService.getStockData(stock);
-    stock.refreshPrice();
+const Stock = require('../models/stock');
+const Option = require('../models/option');
+
+const optionService = require('../services/options.service');
+
+module.exports.test = async function(ticker, price, date){
+    console.log("Line 4 test");
+    let stock = new Stock(ticker); 
+    await stock.refreshPrice();
+    
     console.log(JSON.stringify(stock));
     
     let option = new Option(stock.getSymbol(), stock.getPrice(), date);
     option =  await optionService.getOptionData(option);
+    /*
     console.log(option);
     option.printSummary();
-    
+    */
 }
