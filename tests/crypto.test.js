a27a6cda29bc78afb92a386ea097617b4961526c
Smelt <shivamsatyarthi@gmail.com>
2018-05-11 21:28:05 -0400

    added crypto test

--- tests/crypto.test.js
+++ tests/crypto.test.js
@@ -1,6 +1,21 @@
 
+const Crypto = require('../models/crypto');
+const RMA = require('../scheduler/rma');
+const CryptoService = require('../services/crypto.service');
 
+
+module.exports.rmaTest = async function(ticker = 'ETH'){
+    let ethereum = new Crypto(ticker);
+    RMA.nodeSchedule(ethereum, 4);
+}
+
+moodule.exports.newsTest = async function(ticker = 'ETH'){
+    
+}
+
+/*
 let bitcoin = new Crypto('BTC');
 rma.nodeSchedule(bitcoin, 3);
 let ethereum = new Crypto('ETH');
 rma.nodeSchedule(ethereum, 3);
+*/
