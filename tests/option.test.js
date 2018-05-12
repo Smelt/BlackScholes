593b067b40e8a372baf8e34cf9a0c0b1afb1b49b
Smelt <shivamsatyarthi@gmail.com>
2018-05-11 21:29:57 -0400

    refactored stock test to use defaults

--- tests/option.test.js
+++ tests/option.test.js
@@ -3,21 +3,10 @@ const Option = require('../models/option');
 
 const optionService = require('../services/options.service');
 
-module.exports.testOptionData = async function (ticker, date = '2018-05-18') {
-    
-    if (ticker) {
+module.exports.testOptionData = async function (ticker = 'AAPL', date = '2018-05-18') {
         let stock = new Stock(ticker);
         await stock.refreshPrice();
         let option = new Option(stock.getSymbol(), stock.getPrice(), date);
         await option.refreshPrice()
-        option.printSummary();
-    }
-    else {
-        const monthlyDate = '2018-05-18';
-        let apple = new Stock('aapl');
-        await apple.refreshPrice();
-        let appleOption = new Option(apple.getSymbol(), apple.getPrice(), monthlyDate);
-        await appleOption.refreshPrice();
-        appleOption.printSummary();
-    }
+        option.printSummary();  
 }
