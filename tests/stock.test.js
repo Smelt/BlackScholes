f63fde9dd5272882ab4fc4e31a21ac43a49347ab
Smelt <shivamsatyarthi@gmail.com>
2018-05-11 21:05:34 -0400

    added test for news

--- tests/stock.test.js
+++ tests/stock.test.js
@@ -3,8 +3,14 @@ const Stock = require('../models/stock');
 const rma = require('../scheduler/rma');
 
 
-module.exports.stockPrice = async function(ticker){
+module.exports.stockPrice = async function(ticker = 'AAPL'){
     let stock = new Stock(ticker);
     await stock.refreshPrice();
     rma.nodeSchedule(stock, 3);
+}
+
+module.exports.testNews = async function(ticker = 'AAPL'){
+    let stock = new Stock(ticker);
+    await stock.refreshNews();
+    console.log(JSON.stringify(stock.getNews()));
 }
